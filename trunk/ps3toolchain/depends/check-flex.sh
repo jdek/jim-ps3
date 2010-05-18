00e1eacf784e87adb956c26075832da340768335
oopo <oopo@f0b538bf-8814-0410-9cf2-05af43a94efb>
2010-05-18 12:01:03 +0000

    Updated to the latest versions of binutils, gcc and newlib.

--- trunk/ps3toolchain/depends/check-flex.sh
+++ trunk/ps3toolchain/depends/check-flex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# check-flex.sh by Dan Peori (danpeori@oopo.net)
+# check-flex.sh by Dan Peori (dan.peori@oopo.net)
 
- ## Check for flex.
- flex --version 1> /dev/null || { echo "ERROR: Install flex before continuing."; exit 1; }
+## Check for flex.
+flex --version 1> /dev/null || { echo "ERROR: Install flex before continuing."; exit 1; }
--- trunk/ps3toolchain/depends/check-libgmp3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# check-libgmp3.sh by Dan Peori (danpeori@oopo.net)
-
- ## Check for libgmp3.
- ( ls -l /usr/include/gmp.h || ls -l /usr/include/local/gmp.h ) 1> /dev/null 2> /dev/null || { echo "ERROR: Install libgmp3 before continuing."; exit 1; }
